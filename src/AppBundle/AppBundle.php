22aa927541c79a1b80fb3859a59d84f7f6f48a54
Kirill Kholodilin <charlie@chrl.ru>
2016-09-16 01:22:18 +0200

    Fix codestyle

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,27 @@
 <?php
-
+/**
+ * Main bundle of the app
+ *
+ * PHP Version 5
+ *
+ * @category Symfony
+ * @package  Chrl_Buktopuha
+ * @author   Kirill Kholodilin <charlie@chrl.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://take2.ru/
+ */
 namespace AppBundle;
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
+/**
+ * Bundle class
+ *
+ * @class    AppBundle
+ * @category Symfony
+ * @package  Chrl_Buktopuha
+ * @author   Kirill Kholodilin <charlie@chrl.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://take2.ru/
+ */
 class AppBundle extends Bundle
 {
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,15 +1,39 @@
 <?php
-
+/**
+ * Buktopuha - Telegram bot for russian trivia game
+ *
+ * PHP Version 5
+ *
+ * @category Symfony
+ * @package  Chrl_Buktopuha
+ * @author   Kirill Kholodilin <charlie@chrl.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://take2.ru/
+ */
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+/**
+ * Default route controller - shows homepage and status
+ *
+ * @category Symfony
+ * @package  Chrl_Buktopuha
+ * @author   Kirill Kholodilin <charlie@chrl.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://take2.ru/
+ */
 class DefaultController extends Controller
 {
     /**
+     * Index action -- homepage
+     *
+     * @param Request $request Request object of the webapp
+     *
      * @Route("/", name="homepage")
+     *
+     * @return \HttpResponse
      */
     public function indexAction(Request $request)
     {
