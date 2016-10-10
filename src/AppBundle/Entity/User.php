e1319796eb807556487af4ea7664e5decf3c1230
Kirill Kholodilin <kirill.kholodilin@hitmeister.de>
2016-10-10 18:13:25 +0200

    + Add user entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,15 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chrl
- * Date: 10/10/16
- * Time: 17:36
- */
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Class User
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
 class User
 {
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
+    /**
+     * @param mixed $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChatId()
+    {
+        return $this->chatId;
+    }
+
+    /**
+     * @param int $chatId
+     * @return User
+     */
+    public function setChatId($chatId)
+    {
+        $this->chatId = $chatId;
+        return $this;
+    }
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="chat_id", type="bigint", nullable="false")
+     */
+    protected $chatId;
 }
