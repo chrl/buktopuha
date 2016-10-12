ecc3b382d65c3e15e6e644ad5f8b3b7e0794ad7b
Kirill Kholodilin <kirill.kholodilin@hitmeister.de>
2016-10-12 10:45:11 +0200

    * Fix coding style

--- src/AppBundle/UpdateReceiver/UpdateReceiver.php
+++ src/AppBundle/UpdateReceiver/UpdateReceiver.php
@@ -6,37 +6,36 @@ use Shaygan\TelegramBotApiBundle\TelegramBotApi;
 use Shaygan\TelegramBotApiBundle\Type\Update;
 use Shaygan\TelegramBotApiBundle\UpdateReceiver\UpdateReceiverInterface;
 
-
 class UpdateReceiver implements UpdateReceiverInterface
 {
 
-	private $config;
-	private $telegramBotApi;
+    private $config;
+    private $telegramBotApi;
 
-	public function __construct(TelegramBotApi $telegramBotApi, $config)
-	{
-		$this->telegramBotApi = $telegramBotApi;
-		$this->config = $config;
-	}
+    public function __construct(TelegramBotApi $telegramBotApi, $config)
+    {
+        $this->telegramBotApi = $telegramBotApi;
+        $this->config = $config;
+    }
 
-	public function handleUpdate(Update $update)
-	{
-		$message = json_decode(json_encode($update->message), true);
+    public function handleUpdate(Update $update)
+    {
+        $message = json_decode(json_encode($update->message), true);
 
-		switch ($message['text']) {
-			case "/about":
-			case "/about@{$this->config['bot_name']}":
-				$text = "I'm a Buktopuha Telegram Bot";
-				break;
-			case "/help":
-			case "/help@{$this->config['bot_name']}":
-			default :
-				$text = "Command List:\n";
-				$text .= "/about - About this bot\n";
-				$text .= "/help - show this help message\n";
-				break;
-		}
+        switch ($message['text']) {
+            case "/about":
+            case "/about@{$this->config['bot_name']}":
+                $text = "I'm a Buktopuha Telegram Bot";
+                break;
+            case "/help":
+            case "/help@{$this->config['bot_name']}":
+            default:
+                $text = "Command List:\n";
+                $text .= "/about - About this bot\n";
+                $text .= "/help - show this help message\n";
+                break;
+        }
 
-		$this->telegramBotApi->sendMessage($message['chat']['id'], $text);
-	}
+        $this->telegramBotApi->sendMessage($message['chat']['id'], $text);
+    }
 }
