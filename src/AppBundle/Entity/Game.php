12e0d48f37c093905c950c1849987de2514aeeb3
Kirill Kholodilin <charlie@chrl.ru>
2016-10-23 01:17:39 +0200

    - Remove unneeded copypaste

--- src/AppBundle/Entity/Game.php
+++ src/AppBundle/Entity/Game.php
@@ -14,41 +14,6 @@ use Doctrine\ORM\Mapping as ORM;
 class Game
 {
     /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     * @return Game
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getChatId()
-    {
-        return $this->chatId;
-    }
-
-    /**
-     * @param int $chatId
-     * @return User
-     */
-    public function setChatId($chatId)
-    {
-        $this->chatId = $chatId;
-        return $this;
-    }
-    /**
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -61,12 +26,7 @@ class Game
      * @ORM\Column(name="chat_id", type="bigint", nullable=false)
      */
     protected $chatId;
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="text")
-     */
-    protected $name;
+
     /**
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\User", mappedBy="gameId")
      */
