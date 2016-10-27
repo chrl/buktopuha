bb753b414f9ffedf8ab16b29ca0bc974fb159300
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-10-27 10:48:18 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Chrl/AppBundle/GameAction/GamesAction.php
+++ src/Chrl/AppBundle/GameAction/GamesAction.php
@@ -20,11 +20,11 @@ class GamesAction extends BaseGameAction implements GameActionInterface
 
         /** @var Game $game */
         foreach ($games as $game) {
-            $text.='- '.$game->title."\n";
+            $text .= '- '.$game->title."\n";
         }
 
-        if (count($games)==0) {
-            $text.='- No active games, sorry. Invite me into group, and press /start';
+        if (count($games) == 0) {
+            $text .= '- No active games, sorry. Invite me into group, and press /start';
         }
 
         $this->botApi->sendMessage($message['chat']['id'], $text);
