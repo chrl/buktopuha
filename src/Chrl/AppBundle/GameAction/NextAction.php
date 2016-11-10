a82caf0663c8e4d6b71a4ec2a3ec32089f7e243b
Kirill Kholodilin <kirill.kholodilin@hitmeister.de>
2016-11-10 18:29:59 +0100

    + Add next command

--- src/Chrl/AppBundle/GameAction/NextAction.php
+++ src/Chrl/AppBundle/GameAction/NextAction.php
@@ -6,15 +6,15 @@ class NextAction extends BaseGameAction implements GameActionInterface
 {
     public function run($message, $user)
     {
-		$game = $this->gameService->findGame($message);
+        $game = $this->gameService->findGame($message);
 
-		$this->botApi->sendMessage(
-			$game->chatId,
-			'Ok, changing question.',
-			'markdown'
-		);
-		$this->gameService->em->persist($game);
-		$this->gameService->em->flush();
-		$this->gameService->askQuestion($game);
+        $this->botApi->sendMessage(
+            $game->chatId,
+            'Ok, changing question.',
+            'markdown'
+        );
+        $this->gameService->em->persist($game);
+        $this->gameService->em->flush();
+        $this->gameService->askQuestion($game);
     }
 }
